3dd7e3ed5317fb747e88dcff23321244557fc82b
hollyheroandru <55956192+hollyheroandru@users.noreply.github.com>
2021-04-17 12:14:43 +0300

    added notnull/notempty annotations for fields

--- services/spring-boot/src/main/java/com/egorhristoforov/eventikrestapi/dtos/requests/admin/AdminUserCreateRequest.java
+++ services/spring-boot/src/main/java/com/egorhristoforov/eventikrestapi/dtos/requests/admin/AdminUserCreateRequest.java
@@ -1,22 +1,30 @@
 package com.egorhristoforov.eventikrestapi.dtos.requests.admin;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class AdminUserCreateRequest {
+    @NotNull(message = "Name cannot be null")
     @Size(min = 2, max = 50, message = "Name length must be greater than or equal 6 and less than or equal to 50")
     private String name;
 
+    @NotNull(message = "Surname cannot be null")
     @Size(min = 2, max = 50, message = "Name length must be greater than or equal 6 and less than or equal to 50")
     private String surname;
 
+    @NotNull(message = "Email cannot be null")
     @Size(min = 6, max = 50, message = "Email length must be greater than or equal 6 and less than or equal to 50")
     @Email
     private String email;
 
+    @NotNull(message = "Password cannot be null")
     @Size(min = 6, max = 50, message = "Password length must be greater than or equal 6 and less than or equal to 50")
     private String password;
 
+    @NotNull(message = "Roles cannot be null")
+    @NotEmpty(message = "Roles cannot be empty")
     private Long[] rolesIds;
 
     public AdminUserCreateRequest(String name, String surname,
